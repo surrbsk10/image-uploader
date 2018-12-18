cbf6112614f9114ed785c6d2a5621f6d1f26b815
Ramvignesh Pasupathy <ramvignesh06@gmail.com>
2018-12-18 09:49:31 +0530

    added progress bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Request from 'superagent';
 import './App.css';
 
 const styles = {
@@ -18,7 +19,8 @@ class App extends Component {
     super();
     this.state = {
       fileNames: [],
-      files:  null
+      files:  null,
+      progressArray: []
     };
 
     this.handleFiles = this.handleFiles.bind(this);
@@ -28,15 +30,31 @@ class App extends Component {
 
   handleFiles({ target }) {
     console.log(target);
+    let { progressArray } = this.state;
     if(target.files.length > 0) {
       let fileNames = Array.from(target.files).map(file => file.name);
-      this.setState({ fileNames, files: target.files });
+      for(let i=0; i<fileNames.length; i++) progressArray.push(0);
+      this.setState({ fileNames, progressArray, files: target.files });
     }
   }
 
   uploadFiles() {
     let { files } = this.state;
-    let xhr = new XMLHttpRequest();
+    for(let i=0; i<files.length; i++) {
+      Request
+        .post('http://192.168.43.9:8080/api/uploadFiles')
+        .set('Access-Control-Allow-Origin', '*')
+        .attach('file', files[i])
+        .on('progress', e => {
+            if(e.direction == 'upload') {
+              let { progressArray } = this.state;
+              progressArray[i] = e.percent;
+              this.setState({ progressArray });
+              console.log(e.direction, 'progress: ', e.percent);
+            }
+         })
+        .end((err, res) => { console.log('Resopsne', res, '\nError', err); });
+    }
   }
 
   shorten(str) {
@@ -47,15 +65,20 @@ class App extends Component {
   }
 
   renderProgressTable() {
+    let { fileNames, progressArray } = this.state;
     return (
       <table>
         <tbody>
           {
-            this.state.fileNames.map((name, index) => (
+            fileNames.map((name, index) => (
               <tr key={index}>
                 <td style={styles.col1}>{ index + 1 }</td>
                 <td style={styles.col2}>{ this.shorten(name) }</td>
-                <td style={styles.col3} className='ProgressBar'></td>
+                <td style={styles.col3} className='ProgressBar'>
+                  <div style={{ width: progressArray[index] + '%' }}>
+                  ||||||
+                  </div>
+                </td>
               </tr>
             ))
           }
@@ -72,7 +95,7 @@ class App extends Component {
             <h3>Upload Image(s)</h3>
               <form>
                 <input type='file' multiple onChange={this.handleFiles} />
-                <input type="button" name="upload" value= "upload" />
+                <input type="button" name="upload" value= "upload" onClick={this.uploadFiles}/>
               </form>
           </div>
           <div className='Bottom'>
